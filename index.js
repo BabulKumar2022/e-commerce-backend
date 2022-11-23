1b53d61b727b622634cc39513c659c3bb1663b86
BabulKumar2022 <bb3583400@gmail.com>
2022-11-23 23:44:30 +0600

    first commit

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log(" DB is connected"))
-.catch((err)=>{console.log(err)})
+.catch((err)=>{console.log(err)});
+
+app.get("/api/test", ()=>{
+    console.log("test is successful");
+})
 
 
-app.listen(500, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log('Backend server is running !');
 })
